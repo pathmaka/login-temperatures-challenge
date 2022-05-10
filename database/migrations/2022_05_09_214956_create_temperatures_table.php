702559d378b86aa950daa4886768ab215b26d75b
Pathmaka <pathmakag@gmail.com>
2022-05-10 09:55:20 +0530

    Temperatures table creation

---
+++ database/migrations/2022_05_09_214956_create_temperatures_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateTemperaturesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('temperatures', function (Blueprint $table) {
+            $table->id();
+            $table->bigInteger('user_id');
+            $table->string('fahrenheit'); 
+            $table->string('celsius');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('temperatures');
+    }
+}
